644c84b9289d0e9ee7535894075c57159ab72d81
Vladimir Morulus <vladimirmorulus@gmail.com>
2016-10-09 14:11:01 +0300

    changes error msg

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 // -*- js -*-
 "use strict";
 
-var Dron = require('./../dron/Dron');//require('dron/Dron');
+var Dron = require('dron/Dron');//require('./../dron/Dron');//
 var chalk = require('chalk');
 var argv = require('minimist')(process.argv.slice(2));
 var cli = new Dron(process, argv);
-console.log(chalk.blue('Executes'),chalk.blue.bold(process.argv[2]), chalk.blue('dron'));
+//console.log(chalk.blue('Executes'),chalk.blue.bold(process.argv[2]), chalk.blue('dron'));
 cli.usePackage(process.argv[2], argv)
 .then(function(result) {
 	console.log(result ? chalk.green('Mission complete') : chalk.red('Mission aborted', result));
 })
 .catch(function(e) {
 	if (argv['show-errors']) {
-		console.log(chalk.red('Package '+process.argv[2]+' has an errors'), e);
+		console.log(chalk.red('Package '+process.argv[2]+' has an errors'), e.message, e.stack);
 		throw e;
 	} else {
 		console.log(chalk.red('Package '+process.argv[2]+' has an errors. Run `dron debug '+process.argv[2]+'` to find a problem.'));
