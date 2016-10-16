e7d500d66f52772caccdcafc6ab6e02f9566b4e6
Vladimir Morulus <vladimirmorulus@gmail.com>
2016-10-16 16:24:34 +0300

    change final report to OK

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 // -*- js -*-
 "use strict";
 
-var Dron = require('dron/Dron');//require('./../dron/Dron');//
+var Dron = require('dron/Dron');//require('./../dron/Dron');
 var chalk = require('chalk');
 var argv = require('minimist')(process.argv.slice(2));
 var cli = new Dron(process, argv);
 //console.log(chalk.blue('Executes'),chalk.blue.bold(process.argv[2]), chalk.blue('dron'));
 cli.usePackage(process.argv[2], argv)
 .then(function(result) {
-	console.log(result ? chalk.green('Mission complete') : chalk.red('Mission aborted', result));
+	console.log(result ? chalk.green('OK') : chalk.red('Mission aborted', result));
 })
 .catch(function(e) {
 	if (argv['show-errors']) {
