7ea5f0df18ba17814393e2b931f5b1baec185813
Vladimir Morulus <vladimirmorulus@gmail.com>
2016-11-05 00:49:14 +0300

    conv argv to camelcase

--- index.js
+++ index.js
@@ -2,17 +2,37 @@
 // -*- js -*-
 "use strict";
 
-var Dron = require('dron/Dron');//require('./../dron/Dron');////;
-var chalk = require('chalk');
-var argv = require('minimist')(process.argv.slice(2));
-var cli = new Dron(process, argv);
-//console.log(chalk.blue('Executes'),chalk.blue.bold(process.argv[2]), chalk.blue('dron'));
+const Dron = require('dron/Dron');//require('./../dron/Dron');
+const chalk = require('chalk');
+const argv = require('minimist')(process.argv.slice(2));
+const cli = new Dron(process, argv);
+/**
+ * Convert argv keys to camelCase
+ */
+function camelize(txt) {
+	return txt.replace(/-([\da-z])/gi, function( all, letter ) {
+		return letter.toUpperCase();
+	});
+};
+
+var camelCasedArgv = {};
+for (let prop in argv) {
+	if (argv.hasOwnProperty(prop)&&prop!=='_') {
+		camelCasedArgv[camelize(prop)] = argv[prop];
+	}
+}
+
+/**
+ * Save origin props names until ver. 0.2.0
+ */
+Object.assign(argv, camelCasedArgv);
+
 cli.usePackage(process.argv[2], argv)
 .then(function(result) {
 	console.log(result ? chalk.green('OK') : chalk.red('Mission aborted', result));
 })
 .catch(function(e) {
-	if (argv['show-errors']) {
+	if (argv['displayErrors']) {
 		console.log(chalk.red('Package '+process.argv[2]+' has an errors'), e.message, e.stack);
 		throw e;
 	} else {
