d3ce614151f19c1b14a798c1cafd03c13d3b15e9
Vladimir Morulus <vladimirmorulus@gmail.com>
2016-11-23 02:10:37 +0300

    stable

--- index.js
+++ index.js
@@ -1,41 +1,14 @@
 #! /usr/bin/env node
 // -*- js -*-
 "use strict";
+const path = require('path');
+const dron = require(process.env.DEV?"./../dron/index.js":"dron");
+const args = require('minimist')(process.argv.slice(2));
 
-const Dron = require('dron/Dron');//require('./../dron/Dron');
-const chalk = require('chalk');
-const argv = require('minimist')(process.argv.slice(2));
-const cli = new Dron(process, argv);
-/**
- * Convert argv keys to camelCase
- */
-function camelize(txt) {
-	return txt.replace(/-([\da-z])/gi, function( all, letter ) {
-		return letter.toUpperCase();
-	});
-};
-
-var camelCasedArgv = {};
-for (let prop in argv) {
-	if (argv.hasOwnProperty(prop)&&prop!=='_') {
-		camelCasedArgv[camelize(prop)] = argv[prop];
-	}
-}
-
-/**
- * Save origin props names until ver. 0.2.0
- */
-Object.assign(argv, camelCasedArgv);
-
-cli.usePackage(process.argv[2], argv)
+dron.runFile(path.resolve(process.cwd(), process.argv[2]), args)
 .then(function(result) {
-	console.log(result ? chalk.green('OK') : chalk.red('Mission aborted', result));
+  console.log('Completed');
 })
 .catch(function(e) {
-	if (argv['displayErrors']) {
-		console.log(chalk.red('Package '+process.argv[2]+' has an errors'), e.message, e.stack);
-		throw e;
-	} else {
-		console.log(chalk.red('Package '+process.argv[2]+' has an errors. Run `dron debug '+process.argv[2]+'` to find a problem.'));
-	}
+  console.log('Error', e.message, e.stack);
 });
