e7b2d93c8db1dec2554e0de2d58942f770a03711
Владимир Калмыков <morulus@MacBook-Pro-Vladimir.local>
2017-01-27 03:31:03 +0300

    Resolve files from __erector__

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const erector = require(process.env.DEV ? "./../erector/erector.js" : "erector")
 const args = require('minimist')(process.argv.slice(2));
 const chalk = require('chalk');
 const inspector = require('./inspector');
+const findParentDir = require('find-parent-dir');
 
 function echoError(message, stack) {
   console.log(chalk.bgYellow.black.bold(' ERROR :P ') + ' ' + chalk.inverse(' '+message));
@@ -25,6 +26,23 @@ function localFileExists(filename) {
   }
 }
 
+function searchInLocalErector() {
+  return new Promise(function(resolve, reject) {
+    findParentDir(process.cwd(), '__erector__', function (err, dir) {
+      if (err) {
+        reject(err);
+      } else {
+        const filename = path.join(dir, '__erector__', process.argv[2]+'.js');
+        if (localFileExists(filename)) {
+          resolve(filename);
+        } else {
+          reject();
+        }
+      }
+    });
+  });
+}
+
 const app = erector();
 app.use(erector.pwd(process.env.PWD || process.cwd()));
 const filename = path.resolve(process.cwd(), process.argv[2]);
@@ -33,7 +51,15 @@ if (localFileExists(filename)) {
   result = app.run(filename, args);
 } else {
   if (!/[\.]/.test(process.argv[2])) {
-    result = app.runPackage(process.argv[2], args);
+    result = app.runPackage(process.argv[2], args, {
+      autoinstall: false
+    })
+    .catch(function() {
+      return searchInLocalErector(process.argv[2])
+      .then(function(fullname) {
+        return app.run(fullname, args);
+      });
+    });
   } else {
     result = Promise.reject(new Error('Module "'+filename+'" not found'));
   }
