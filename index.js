72dd969bd4ccc3b7494f1e99b88e828b824827f1
Vladimir Kalmykov <v.kalmykov@infotech.team>
2017-07-14 22:57:03 +0300

    #0.2.0-beta.2

--- index.js
+++ index.js
@@ -3,43 +3,56 @@
 "use strict";
 const fs = require('fs');
 const path = require('path');
-const erector = require(process.env.DEV ? "./../erector/erector.js" : "erector");
+const erector = require("./../erector/dist/app.js");
 const args = require('minimist')(process.argv.slice(2));
 const chalk = require('chalk');
-const isFileSync = require('./lib/isFileSync.js');
+const isFileSync = require('./lib/helpers/isFileSync.js');
 const searchInLocalErector = require('./lib/searchInLocalErector.js');
+const executeSystemCommand = require('./lib/helpers/executeSystemCommand.js');
 const inspector = require('./inspector');
 
-function echoError(message, stack) {
-  console.log(chalk.bgYellow.black.bold(' ERROR :P ') + ' ' + chalk.inverse(' '+message));
+function echoError(e) {
+  const message = e.message;
+  const stack = e.stack;
+  console.log(chalk.bgYellow.black.bold('Erector caught'+e.name)
+    + "\n\n"
+    + chalk.bgYellow.black.bold('Location:') + ' ' + chalk.inverse(' '+message));
   let hint = inspector(message, stack);
   console.log(hint ? hint : chalk.dim(stack.substr(stack.indexOf(message)+message.length)));
+  process.exit(0);
 }
 
 const app = erector();
 app.use(erector.pwd(process.env.PWD || process.cwd()));
-const filename = path.resolve(process.cwd(), process.argv[2]);
-let result;
-if (isFileSync(filename)) {
-  result = app.run(filename, args);
-} else {
-  if (!/[\.]/.test(process.argv[2])) {
-    result = app.runPackage(process.argv[2], args, {
-      autoinstall: false
-    })
-    .catch(function(e) {
-      return (e.type === erector.constants.ERR_UNDEFINED_PACKAGE)
-      ? searchInLocalErector(process.argv[2])
-      .then(function(fullname) {
-        return app.run(fullname, args);
-      }) : Promise.reject(e);
-    });
-  } else {
-    result = Promise.reject(new Error('Module "'+filename+'" not found'));
+Promise.resolve(process.argv[2])
+.then(executeSystemCommand)
+.catch(function(payload) {
+  if (payload instanceof Error) {
+    throw payload;
   }
-}
+  const filename = path.resolve(process.cwd(), payload);
+  let result;
 
-result
+  if (isFileSync(filename)) {
+    result = app.run(filename, args);
+  } else {
+    if (!/[\.]/.test(process.argv[2])) {
+      result = app.runPackage(process.argv[2], args, {
+        autoinstall: false
+      })
+      .catch(function(e) {
+        return (e.type === erector.constants.ERR_UNDEFINED_PACKAGE)
+        ? searchInLocalErector(process.cwd(), process.argv[2])
+        .then(function(fullname) {
+          return app.run(fullname, args);
+        }) : Promise.reject(e);
+      });
+    } else {
+      result = Promise.reject(new Error('Module "'+filename+'" not found'));
+    }
+  }
+  return result;
+})
 .catch(function(e) {
-  echoError(e.message, e.stack);
+  echoError(e);
 });
